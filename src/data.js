74b3208619d26d6fcc4de019cfe9f468b6a0a3a1
Münir BESYE <imunirbesye@gmail.com>
2023-11-07 18:26:20 +0300

    yapildi

---
+++ src/data.js
@@ -0,0 +1,136 @@
+export const data = {
+  tr: {
+    intro: [
+      "Frontend Geliştiricisiyim",
+      "İyi kullanıcı deneyimleriyle sağlam ve ölçeklenebilir ürünler oluşturmayı severim",
+    ],
+    profile: {
+      birth: "06.07.1998",
+      location: "Kocaeli",
+      collage: "Hoca Ahmet Yesevi Üniv. Computer Engineering, 2025",
+      prefer: "Fullstack",
+      aboutMe:
+        "Ben Münir BESYE. Konya Ereğli'de 1998'de doğdum. 2016'da Milli Eğitim Bakanlığı'na atandım. 2022 yılında Sosyal Güvenlik Kurumu'na memur kadrosunda geçiş yaptım ve şu anda orada devam etmekteyim. 2023 yılında Workintech ile Full Stack Develeoper Kursu'na başladım ve devam ediyorum.",
+    },
+    projects: [
+      {
+        title: "Portfolyo",
+        description:
+          "Basit, kişiselleştirilebilir, minimal kullanıcının hangi çerezleri kabul edip etmeyeceğini seçmesine yarayan bir çerez sistemi. Bu Vanilla JS, SCSS ve Parcel Bundler kullanılarak oluşturuldu ve NPM paketi olarak mevcut ve Git Repository herhangi bir kode ve tema değişkiliği yapılmasına olanak sağlamaktadır.",
+        techs: ["React", "HTML", "CSS", "Tailwind"],
+        links: [
+          "www.google.com",
+          "https://github.com/imunirbesye/FSWeb-Frontend-Challenge",
+        ],
+        resim: "https://i.ibb.co/9ZbyXFK/portfolyo.png",
+      },
+      {
+        title: "Pizza Sipariş",
+        description:
+          "Basit, kişiselleştirilebilir, minimal kullanıcının hangi çerezleri kabul edip etmeyeceğini seçmesine yarayan bir çerez sistemi. Bu Vanilla JS, SCSS ve Parcel Bundler kullanılarak oluşturuldu ve NPM paketi olarak mevcut ve Git Repository herhangi bir kode ve tema değişkiliği yapılmasına olanak sağlamaktadır.",
+        techs: ["React", "HTML", "CSS"],
+        links: ["https://github.com/imunirbesye/fsweb-s7-challenge-pizza"],
+        resim: "https://i.ibb.co/nbyDNDp/pizza.png",
+      },
+      {
+        title: "Alışveriş Sepeti",
+        description:
+          "Basit, kişiselleştirilebilir, minimal kullanıcının hangi çerezleri kabul edip etmeyeceğini seçmesine yarayan bir çerez sistemi. Bu Vanilla JS, SCSS ve Parcel Bundler kullanılarak oluşturuldu ve NPM paketi olarak mevcut ve Git Repository herhangi bir kode ve tema değişkiliği yapılmasına olanak sağlamaktadır.",
+        techs: ["React", "HTML", "CSS"],
+        links: ["https://github.com/imunirbesye/fsweb-s7-challenge-pizza"],
+        resim: "https://i.ibb.co/j5d4xVn/shoppingcart.png",
+      },
+    ],
+    links: {
+      twitter: "https://twitter.com",
+      instagram: "https://www.instagram.com",
+      github: "https://www.github.com",
+      linkedin: "https://www.linkedin.com",
+    },
+    titles: [
+      "ENGLISH",
+      "Yetenekler",
+      [
+        "Profil",
+        "Temel Bilgiler",
+        "Doğum Tarihi",
+        "İkamet",
+        "Eğitim",
+        "Tercih Edilen Rol",
+        "Hakkımda",
+      ],
+      ["Projeler", "Siteyi Görüntüle"],
+      [
+        "Bana bir mesaj gönder!",
+        "Teklifin veya sorun mu var ya da sadece selam mı vermek istiyorsun? Buyur.",
+      ],
+    ],
+  },
+  en: {
+    intro: [
+      "I am a Frontend Developer...",
+      "...who likes to craft solid and scalable frontend products with great user experiences.",
+    ],
+    profile: {
+      birth: "06.07.1998",
+      location: "Kocaeli",
+      collage: "Hoca Ahmet Yesevi Üniv. Computer Engineering, 2025",
+      prefer: "Fullstack",
+      aboutMe:
+        "I'm Münir BESYE. I was bown in 1998 in Konya, Ereğli. I had worked in Ministry of Education between 2016-2022 and then I have started to work in the Ministry of Labor and Social Security. I've started the Full Stack Developer Course with Workintech in 2023.",
+    },
+    projects: [
+      {
+        title: "Portfolio",
+        description:
+          "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        techs: ["React", "HTML", "CSS", "Tailwind"],
+        links: [
+          "www.google.com",
+          "https://github.com/imunirbesye/FSWeb-Frontend-Challenge",
+        ],
+        resim: "https://i.ibb.co/9ZbyXFK/portfolyo.png",
+      },
+      {
+        title: "Pizza Order",
+        description:
+          "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        techs: ["React", "HTML", "CSS"],
+        links: ["https://github.com/imunirbesye/FSWeb-Frontend-Challenge"],
+        resim: "https://i.ibb.co/nbyDNDp/pizza.png",
+      },
+      {
+        title: "Shopping Cart",
+        description:
+          "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+        techs: ["React", "HTML", "CSS"],
+        links: ["https://github.com/imunirbesye/FSWeb-Frontend-Challenge"],
+        resim: "https://i.ibb.co/j5d4xVn/shoppingcart.png",
+      },
+    ],
+    links: {
+      twitter: "https://twitter.com",
+      instagram: "https://www.instagram.com",
+      github: "https://www.github.com",
+      linkedin: "https://www.linkedin.com",
+    },
+    titles: [
+      "TURKISH",
+      "Skills",
+      [
+        "Profile",
+        "Basic Information",
+        "Date of Birth",
+        "Location",
+        "Degree",
+        "Prefer",
+        "About Me",
+      ],
+      ["Projects", "View Site", "Github"],
+      [
+        "Send me a message!",
+        "Got a question or proposal, or just want to say hello? Go ahead.",
+      ],
+    ],
+  },
+};
